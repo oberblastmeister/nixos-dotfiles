8c9540ca09e5c3d56ca26abb554b6dbd0e8db49f
Brian Shu <littlebubu.shu@gmail.com>
2021-05-30 17:24:38 -0400

    update inputs

--- flake.nix
+++ flake.nix
@@ -1,6 +1,6 @@
 {
-  # inputs.nixpkgs.url = "github:nixos/nixpkgs/nixos-20.09";
-  inputs.nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+  inputs.nixpkgs.url = "github:nixos/nixpkgs/nixos-20.09";
+  /* inputs.nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable"; */
   outputs = { self, nixpkgs }: {
     nixosConfigurations.nixos-desktop = nixpkgs.lib.nixosSystem {
       system = "x86_64-linux";
