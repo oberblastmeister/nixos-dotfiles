88ebe71ddf1d3e38aca575eee22868606208a83d
Brian Shu <littlebubu.shu@gmail.com>
2021-06-05 18:57:10 -0400

    remove

--- home/neovim.nix
+++ home/neovim.nix
@@ -10,19 +10,19 @@
     haskell-language-server
     sumneko-lua-language-server
 
-    nodePackages.typescript-language-server
-    nodePackages.vscode-html-languageserver-bin
-    nodePackages.vscode-css-languageserver-bin
-    nodePackages.vscode-json-languageserver-bin
-    nodePackages.yaml-language-server
-    nodePackages.vim-language-server
-    nodePackages.bash-language-server
+    /* nodePackages.typescript-language-server */
+    /* nodePackages.vscode-html-languageserver-bin */
+    /* nodePackages.vscode-css-languageserver-bin */
+    /* nodePackages.vscode-json-languageserver-bin */
+    /* nodePackages.yaml-language-server */
+    /* nodePackages.vim-language-server */
+    /* nodePackages.bash-language-server */
 
-    texlab
-    clojure-lsp
-    dhall-lsp-server
+    /* texlab */
+    /* clojure-lsp */
+    /* dhall-lsp-server */
 
-    ocamlPackages.ocaml-lsp
-    ocamlformat
+    /* ocamlPackages.ocaml-lsp */
+    /* ocamlformat */
   ];
 }
