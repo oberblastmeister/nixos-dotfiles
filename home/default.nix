c2597d31a34c870b247c2f5c2f704522717dcd36
Brian Shu <littlebubu.shu@gmail.com>
2021-06-05 13:21:29 -0400

    added fonts

--- home/default.nix
+++ home/default.nix
@@ -7,5 +7,17 @@
     ./vscode.nix
   ];
 
+  fonts.fonts = with pkgs; [
+    noto-fonts
+    noto-fonts-emoji
+    (nerdfonts.override {
+      fonts = [
+        FiraCode
+        Iosevka
+        VictorMono
+      ];
+    })
+  ];
+
   home.stateVersion = "21.05";
 }
