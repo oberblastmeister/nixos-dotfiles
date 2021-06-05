72d33cc65a0cd75faa0b621d0a37e294676a31b6
Brian Shu <littlebubu.shu@gmail.com>
2021-06-05 13:45:34 -0400

    added neovim nightly overlay

--- flake.nix
+++ flake.nix
@@ -1,28 +1,38 @@
 {
-  # inputs.nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-  inputs.nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
 
-  inputs.home-manager = {
-    url = "github:nix-community/home-manager";
-    # url = "path:/Users/michael/Repositories/nix/home-manager";
-    inputs.nixpkgs.follows = "nixpkgs";
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      # url = "path:/Users/michael/Repositories/nix/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    neovim-nightly-overlay.url = "github:nix-community/neovim-nightly-overlay";
   };
 
-  outputs = { self, nixpkgs, home-manager, ... }: {
-    nixosConfigurations = {
-      nixos-desktop = nixpkgs.lib.nixosSystem {
-        system = "x86_64-linux";
-        modules = [
-          ./configuration.nix
+  outputs = { self, nixpkgs, home-manager, ... }:
+    let
+      overlays = [
+        inputs.neovim-nightly-overlay.overlay
+      ];
+    in
+    {
+      nixosConfigurations = {
+        nixos-desktop = nixpkgs.lib.nixosSystem {
+          system = "x86_64-linux";
+          nixpkgs.overlays = overlays;
+          modules = [
+            ./configuration.nix
 
-          home-manager.nixosModules.home-manager
-          {
-            home-manager.useGlobalPkgs = true;
-            home-manager.useUserPackages = true;
-            home-manager.users.brian = import ./home;
-          }
-        ];
+            home-manager.nixosModules.home-manager
+            {
+              home-manager.useGlobalPkgs = true;
+              home-manager.useUserPackages = true;
+              home-manager.users.brian = import ./home;
+            }
+          ];
+        };
       };
     };
-  };
 }
