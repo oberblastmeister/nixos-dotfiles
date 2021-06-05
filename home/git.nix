6f9e4aa8362e382304f842a861b77951cbbe4646
Brian Shu <littlebubu.shu@gmail.com>
2021-06-05 13:03:11 -0400

    added git configuration

---
+++ home/git.nix
@@ -0,0 +1,12 @@
+{ config, pkgs, ... }:
+
+{
+  programs.git = {
+    enable = true;
+    userName = "Brian Shu";
+    userEmail = "littlebubu.shu@gmail.com";
+    delta = {
+      enable = true;
+    };
+  };
+}
