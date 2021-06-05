8aeabc2b67bef584b10ea2baaac7dc212b444049
Brian Shu <littlebubu.shu@gmail.com>
2021-06-05 11:29:48 -0400

    added home manager

--- flake.nix
+++ flake.nix
@@ -1,6 +1,13 @@
 {
-  inputs.nixpkgs.url = "github:nixos/nixpkgs/nixos-20.09";
-  /* inputs.nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable"; */
+  # inputs.nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+  inputs.nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+
+  inputs.home-manager = {
+    url = "github:nix-community/home-manager";
+    # url = "path:/Users/michael/Repositories/nix/home-manager";
+    inputs.nixpkgs.follows = "nixpkgs";
+  };
+
   outputs = { self, nixpkgs }: {
     nixosConfigurations.nixos-desktop = nixpkgs.lib.nixosSystem {
       system = "x86_64-linux";
