df670ae6ee644968b6e2a0f2f1626395306ff479
Brian Shu <littlebubu.shu@gmail.com>
2021-06-05 19:09:05 -0400

    remove

--- home/vscode.nix
+++ home/vscode.nix
@@ -1,14 +1,14 @@
 { config, pkgs, ... }:
 
 {
-  programs.vscode = {
-    enable = true;
-    package = pkgs.vscodium;
-    extensions = with pkgs.vscode-extensions; [
-      matklad.rust-analyzer
-      haskell.haskell
-      ms-python.python
-    ] ++ pkgs.vscode-utils.extensionsFromVscodeMarketplace [
-    ];
-  };
+  # programs.vscode = {
+  #   enable = true;
+  #   package = pkgs.vscodium;
+  #   extensions = with pkgs.vscode-extensions; [
+  #     matklad.rust-analyzer
+  #     haskell.haskell
+  #     ms-python.python
+  #   ] ++ pkgs.vscode-utils.extensionsFromVscodeMarketplace [
+  #   ];
+  # };
 }
