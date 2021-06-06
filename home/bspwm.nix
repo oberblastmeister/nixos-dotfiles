70ec6c72431f6e5a1a0c010ad6144dc10dd784a0
Brian Shu <littlebubu.shu@gmail.com>
2021-06-06 15:04:28 -0400

    fix

--- home/bspwm.nix
+++ home/bspwm.nix
@@ -1,12 +1,6 @@
 { config, pkgs, ... }:
 
 {
-  programs = {
-    bspwm = {
-      enable = true;
-    };
-  };
-
   xdg.configFile = {
     "bspwm".source = ../files/.config/bspwm;
     "sxhkd".source = ../files/.config/sxhkd;
